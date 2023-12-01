154a3bc406e3ba1f7e71b08e6b350866f4d0f842
Frank Schmidt <frank.schmidt@codecentric.de>
2023-12-02 00:59:22 +0100

    Day 1 part 2
    stupid workaround: since numbers could share letters, instead of
    refactoring my solution I just ensured that those combinations still
    exist by duplicating the number around the newly added int.

--- src/Day1/Program.fs
+++ src/Day1/Program.fs
@@ -17,5 +17,35 @@ let part1 (input: string) =
 
 printfn "part1: %d" (part1 real_input)
 
-let part2 (input: string) = 0
+let part2 (input: string) =
+    input.Split("\n", System.StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (fun s -> s.Replace("one", "one1one"))
+    |> Array.map (fun s -> s.Replace("two", "two2two"))
+    |> Array.map (fun s -> s.Replace("three", "three3three"))
+    |> Array.map (fun s -> s.Replace("four", "four4four"))
+    |> Array.map (fun s -> s.Replace("five", "five5five"))
+    |> Array.map (fun s -> s.Replace("six", "six6six"))
+    |> Array.map (fun s -> s.Replace("seven", "seven7seven"))
+    |> Array.map (fun s -> s.Replace("eight", "eight8eight"))
+    |> Array.map (fun s -> s.Replace("nine", "nine9nine"))
+    |> Array.map extractNumber
+    // |> Array.map (fun s ->
+    // printfn "%d" s
+    // s)
+    |> Array.sum
+
+printfn
+    "part2 test: 281 == %d"
+    (part2
+        """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+""")
+
+
 printfn "part2: %d" (part2 real_input)
