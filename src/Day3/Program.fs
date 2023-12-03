ae649ab807bd9007e5460cec6a93a4b3c46c4e22
Frank Schmidt <frank.schmidt@codecentric.de>
2023-12-03 22:01:42 +0100

    Day3 part 2 done, just very slow

--- src/Day3/Program.fs
+++ src/Day3/Program.fs
@@ -28,39 +28,38 @@ let test_lines = splitLines test_input
 
 type Point = { line: int; col: int }
 
-type Number =
+type Number = { start: Point; length: int; num: int }
+
+type Symbol =
     { start: Point
       length: int
-      value: int }
+      value: string }
 
-let findSymbols (input: array<string>) : Set<Point> =
-    let mutable points = []
-
-    for l in 0 .. input.Length - 1 do
-        let line = input.[l]
-
-        for c in 0 .. line.Length - 1 do
-            let char = line.Chars(c)
-
-            if (not (List.contains char ('.' :: [ '0' .. '9' ]))) then
-                points <- { line = l; col = c } :: points
-
-    Set.ofList points
-
-printfn "symbols: %A" (findSymbols test_lines)
-
-let findNumbers (input: array<string>) =
-
+let find (regex: string) (input: array<string>) : seq<Symbol> =
     seq {
         for l in 0 .. input.Length - 1 do
             let line = input.[l]
 
-            for rmatch in Regex.Matches(line, "[0-9]+") do
+            for rmatch in Regex.Matches(line, regex) do
                 yield
                     { start = { line = l; col = rmatch.Index }
                       length = rmatch.Length
-                      value = Util.parseInt rmatch.Value |> Option.get }
+                      value = rmatch.Value }
     }
+
+let findSymbols (input: array<string>) : Set<Point> =
+    find "[^0-9.]" input
+    |> Seq.map (fun symbol -> symbol.start)
+    |> Set.ofSeq
+
+printfn "symbols: %A" (findSymbols test_lines)
+
+let findNumbers (input: array<string>) =
+    find "[0-9]+" input
+    |> Seq.map (fun symbol ->
+        { start = symbol.start
+          length = symbol.length
+          num = Util.parseInt symbol.value |> Option.get })
 
 printfn "%A" (findNumbers test_lines)
 
@@ -87,15 +86,31 @@ let partNumbers (numbers: seq<Number>) symbols =
 
 let part1 (input: array<string>) =
     partNumbers (findNumbers input) (findSymbols input)
-    |> Seq.map (fun number -> number.value)
+    |> Seq.map (fun number -> number.num)
     |> Seq.sum
 
 printfn "part1 test: 4361 == %d" (part1 test_lines)
 printfn "part1: %d" (part1 real_lines)
 
-let part2 (input: string) = 0
+let findGearRatios (parts: seq<Number>) (input: array<string>) =
+    find "\*" input
+    |> Seq.map (fun gear ->
+        parts
+        |> Seq.filter (fun number ->
+            Set.intersect (neighbors number.start number.length) (Set.ofList [ gear.start ])
+            |> Set.isEmpty
+            |> (not)))
+    |> Seq.filter (fun adjacent -> Seq.length adjacent = 2)
+    |> Seq.map (fun adjacent ->
+        adjacent
+        |> Seq.map (fun number -> number.num)
+        |> Seq.reduce (*))
 
-printfn "part2 test: 467835 = %d" (part2 test_input)
+let part2 (input: array<string>) =
+    let parts = partNumbers (findNumbers input) (findSymbols input)
+    findGearRatios parts input |> Seq.sum
+
+printfn "part2 test: 467835 = %d" (part2 test_lines)
 
 
-printfn "part2: %d" (part2 real_input)
+printfn "part2: %d" (part2 real_lines)
