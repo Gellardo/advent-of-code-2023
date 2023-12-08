900831ff18537bb1feb7adcf8ae3af804f2ddcdf
Frank Schmidt <frank.schmidt@codecentric.de>
2023-12-08 15:52:37 +0100

    Finished d8p1

--- src/Day8/Program.fs
+++ src/Day8/Program.fs
@@ -27,9 +27,9 @@ let parseRules (rules: string) =
     )
     |> Array.map (fun line ->
         let matches = Regex.Matches(line, "([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)")
-        let from = matches[0].Groups[0]
-        let left = matches[0].Groups[1]
-        let right = matches[0].Groups[2]
+        let from = matches[0].Groups[1]
+        let left = matches[0].Groups[2]
+        let right = matches[0].Groups[3]
         (from.Value, (left.Value, right.Value)))
     |> Map.ofArray
 
@@ -58,6 +58,7 @@ let part1 (pattern, rules: Map<string, (string * string)>) =
         let mutable steps: int = 0
 
         for instruction in pattern do
+            // printfn "%s %s %A" state instruction rules
             state <-
                 match instruction with
                 | "L" -> fst rules[state]
@@ -76,9 +77,48 @@ let part1 (pattern, rules: Map<string, (string * string)>) =
 printfn "part1 test: 6 == %d" (part1 test_lines)
 printfn "part1: %d" (part1 real_lines)
 
-let part2 (input) =
+let part2 (pattern, rules: Map<string, (string * string)>) =
     // input |> Array.map parseLine |> Array.sum
-    1
+    seq {
+        let mutable states: list<string> =
+            rules.Keys
+            |> Seq.filter (fun state -> state.EndsWith "A")
+            |> Seq.toList
 
-printfn "part2 test: YYY = %d" (part2 test_lines)
+        let mutable steps: int = 0
+
+        for instruction in pattern do
+            // printfn "%s %s %A" state instruction rules
+            states <-
+                states
+                |> List.map (fun state ->
+                    match instruction with
+                    | "L" -> fst rules[state]
+                    | "R" -> snd rules[state]
+                    | _ -> failwith "unknown pattern")
+
+            steps <- steps + 1
+
+            if states
+               |> List.forall (fun state -> state.EndsWith "Z") then
+                yield steps
+    }
+    |> Seq.head
+
+let test_lines_new =
+    splitLines
+        """
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+"""
+
+printfn "part2 test: 6 = %d" (part2 test_lines)
 printfn "part2: %d" (part2 real_lines)
