cd57fbdb78b3d108345d4fa80adfc5c23d074ac6
Frank Schmidt <frank.schmidt@codecentric.de>
2023-12-08 15:35:31 +0100

    Day8p1 should work

--- src/Day8/Program.fs
+++ src/Day8/Program.fs
@@ -14,17 +14,24 @@ ZZZ = (ZZZ, ZZZ)
 let toSeq (pattern: string) =
     seq {
         let instructions = Util.chars pattern
+
         while true do
             yield! instructions
     }
 
 let parseRules (rules: string) =
     rules.Split(
-        "\n\n",
+        "\n",
         System.StringSplitOptions.RemoveEmptyEntries
         ||| System.StringSplitOptions.TrimEntries
     )
-    |> Array.map (fun line -> line)
+    |> Array.map (fun line ->
+        let matches = Regex.Matches(line, "([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)")
+        let from = matches[0].Groups[0]
+        let left = matches[0].Groups[1]
+        let right = matches[0].Groups[2]
+        (from.Value, (left.Value, right.Value)))
+    |> Map.ofArray
 
 let splitLines (input: string) =
     match
@@ -36,34 +43,42 @@ let splitLines (input: string) =
             )
         with
     | [| pattern: string; rules: string |] -> ((toSeq pattern), (parseRules rules))
-    | _ -> failwith ("could not parse lines")
+    | a -> failwith (sprintf "could not parse lines: %A" a)
 
 
 let real_lines = splitLines real_input
+printfn "%A" real_lines
 let test_lines = splitLines test_input
 
 let parseLine s = 1
 
-let part1 (pattern, rules) =
-    let mut state = "AAA"
-    let mut steps = 0
-    seq{
-    for instruction in pattern do
-        state <- rules[state][instruction]
-        steps <- steps + 1
-        if state = "ZZZ" then
-            yield steps
-    } |> Seq.take 1
+let part1 (pattern, rules: Map<string, (string * string)>) =
+    seq {
+        let mutable state: string = "AAA"
+        let mutable steps: int = 0
 
-    pattern |> Seq.fold (fun acc, instruction -> acc) "AAA"
-    // "AAA" |> Array.map parseLine |> Array.sum
-    1
+        for instruction in pattern do
+            state <-
+                match instruction with
+                | "L" -> fst rules[state]
+                | "R" -> snd rules[state]
+                | _ -> failwith "unknown pattern"
+
+            steps <- steps + 1
+            if state = "ZZZ" then yield steps
+    }
+    |> Seq.head
+
+
+// pattern |> Seq.fold (fun acc, instruction -> acc) "AAA"
+// "AAA" |> Array.map parseLine |> Array.sum
 
 printfn "part1 test: 6 == %d" (part1 test_lines)
 printfn "part1: %d" (part1 real_lines)
 
-let part2 (input: array<string>) =
-    input |> Array.map parseLine |> Array.sum
+let part2 (input) =
+    // input |> Array.map parseLine |> Array.sum
+    1
 
 printfn "part2 test: YYY = %d" (part2 test_lines)
 printfn "part2: %d" (part2 real_lines)
